475ac6a446cd633245eadd5ee9adea618fa2beab
Hammad Majid <hm.hammadmajid@gmail.com>
2022-10-11 06:08:28 +0000

    calculate dimes

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ def main():
     quarters = calculate_quarters(cents)
     cents = cents - quarters * 25
 
+    dimes = calculate_dimes(cents)
+    cents = cents - dimes * 10
+
 
 def read_cents():
     change = 0
@@ -25,6 +28,14 @@ def calculate_quarters(cents):
         quarter_coins += 1
         cents -= 25
     return quarter_coins
+
+
+def calculate_dimes(cents):
+    dime_coins = 0
+    while cents >= 10:
+        dime_coins += 1
+        cents -= 10
+    return dime_coins
 
 
 main()
