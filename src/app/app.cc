13f91e3c2e62ede731dca953543f11c0c50d451c
hammadmajid <hm.hammadmajid@gmail.com>
2022-11-28 20:24:23 +0500

    Implement get_cents() functions
    Reads change from user and than converts it to cents
    
    if user doesn't enter valid input than loop will start over

--- src/app/app.cc
+++ src/app/app.cc
@@ -6,3 +6,23 @@ int main()
     int cents = greedy_algorithm::get_cents();
     return 0;
 }
+
+int greedy_algorithm::get_cents()
+{
+    float change = 0;
+
+    while (true)
+    {
+        std::cout << std::endl
+                  << "Change owed: ";
+
+        std::cin >> change;
+
+        if (change > 0)
+        {
+            break;
+        }
+    }
+
+    return round(change * 100);
+}
