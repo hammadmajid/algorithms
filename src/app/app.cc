a62cf82d86c5904c270176c99dadcd8002f40d31
HappyBird <101904551+HappyBirdisAXE@users.noreply.github.com>
2022-11-29 19:12:12 +0000

    Created safe code to avoid infinite loop

--- src/app/app.cc
+++ src/app/app.cc
@@ -19,6 +19,16 @@ int greedy_algorithm::get_cents()
 
         std::cin >> change;
 
+        if(std::cin.fail())
+        {
+            //Tells user that their input was incorrect, then clears the stream for 
+            //another attempt until user inputs correct information
+            std::cout << "You've enter an incorrect input" << std::endl;
+
+            std::cin.clear();
+            std::cin.ignore(__INT_MAX__, '\n');
+        }
+
         if (change > 0)
         {
             break;
