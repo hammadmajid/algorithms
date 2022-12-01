53fed8e9a9f5825f092dba6416e68cd52a721ba6
Hammad Majid <hm.hammadmajid@gmail.com>
2022-12-01 17:41:43 +0000

    Update app.cc file based on newly created module

--- src/app/app.cc
+++ src/app/app.cc
@@ -1,39 +1,7 @@
-#include <iostream>
-#include <math.h>
-#include "header/app.h"
+#include "user.cc"
 
 int main()
 {
     int cents = greedy_algorithm::get_cents();
     return 0;
-}
-
-int greedy_algorithm::get_cents()
-{
-    float change = 0;
-
-    while (true)
-    {
-        std::cout << std::endl
-                  << "Change owed: ";
-
-        std::cin >> change;
-
-        if(std::cin.fail())
-        {
-            //Tells user that their input was incorrect, then clears the stream for 
-            //another attempt until user inputs correct information
-            std::cout << "You've enter an incorrect input" << std::endl;
-
-            std::cin.clear();
-            std::cin.ignore(__INT_MAX__, '\n');
-        }
-
-        if (change > 0)
-        {
-            break;
-        }
-    }
-
-    return round(change * 100);
 }
