a5c255d92d8a2da761ac01c9247e5cba42e194e0
hammadmajid <hm.hammadmajid@gmail.com>
2022-12-05 20:14:21 +0500

    Document code and style it

--- greedy-algorithm/src/input.cc
+++ greedy-algorithm/src/input.cc
@@ -4,25 +4,24 @@
 #include <math.h>
 #include <string.h>
 
-bool Input::IsPositiveRealNumber(const char flag[])
+bool Input::IsPositiveRealNumber(const char input[])
 {
-    int len = strlen(flag);
+    // TODO(@hammadmajid) return false if here are more than one dots(.) in flag
+
+    int len = strlen(input);
     for (int i = 0; i < len; i++ )
     {
-        if (!std::isdigit(flag[i]) || flag[i] != 46) // 46 in ASCII represents the dot(.) character
+        if (!std::isdigit(input[i]) || input[i] != 46) // 46 in ASCII represents the dot(.) character
         {
             return false;
         }
     }
-
     return true;
 }
-
-int Input::ConvertInputToCents(const char change[])
+int Input::ConvertInputToCents(const char input[])
 {
-    return std::round(std::atof(change) * 100);
+    return std::round(std::atof(input) * 100);
 }
-
 void Input::ExitProgram(const char message[], int exit_code)
 {
     std::cout << std::endl
--- greedy-algorithm/src/input.hh
+++ greedy-algorithm/src/input.hh
@@ -1,32 +1,51 @@
 #ifndef INPUT_H
 #define INPUT_H
 
+/*
+    TODO(@hammadmajid) Change this class to namespace
+     - Class is being used as namespace
+     - No instance of this class is ever initalized
+*/
 class Input
 {
 public:
-    Input() = default;
 
     /**
-     * @brief TODO
+     * @brief We are converting the input that was provided by user to cents
+     * Input has type `char` because input was read from argv
+     * Why convert? algorithm::calculate(int cents) will require cents in type `int`
+     * Also, we are assuming that input is provided in dollars($)
+     * Steps to convert:
+     * 1. First input is converted from char[] to float
+     * 2. The float value is multiplied 100 to convert to cents, since 100 cents = 1 dollar
+     * 3. The multiplied value to rounded off, aka converted to int
      *
-     * @param change
-     * @return int
+     * @param change will be converted
+     * @return change that was converted
      */
-    static int ConvertInputToCents(const char change[]);
+    static int ConvertInputToCents(const char input[]);
+
     /**
-     * @brief Verifies that the supplied flag is positive real(ℝ) number
+     * @brief Verifies that the supplied input is a positive real(ℝ) number
+     * The method will return true if all character matches these cases
+     *  - Character is a digit (0-9)
+     *  - At most one character is a dot(.)
      *
-     * @param flag Will verify this is positive real number
-     * @return true if the flag is positive real number
-     * @return false if the flag is not positive real number
+     * @param input Will be checked for above cases
+     * @return true if the input is positive real number
+     * @return false if the input is not positive real number
      */
-    static bool IsPositiveRealNumber(const char flag[]);
+    static bool IsPositiveRealNumber(const char input[]);
+
+    /*
+        TODO(@hammadmajid) move exit program method to main.cc
+        Because it doesn't make sense to put it in class called `Input`
+    */
     /**
-     * @brief When invoked this method will cause the program to exit
-     * with supplied message and exit code
+     * @brief This method will used to exit out the program when something goes wrong.
      *
-     * @param message this message will be displayed on console before exiting
-     * @param exit_code the program will exit with this code
+     * @param message will be displayed on console before exiting
+     * @param exit_code should non-zero and unique
      */
     static void ExitProgram(const char message[], int exit_code);
 };
