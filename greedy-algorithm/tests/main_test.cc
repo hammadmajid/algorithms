eeee32680162e21cf5f540ff10c7a659a04ffd73
hammadmajid <hm.hammadmajid@gmail.com>
2022-12-07 20:32:36 +0500

    Call tests

---
+++ greedy-algorithm/tests/main_test.cc
@@ -0,0 +1,10 @@
+#include "input_test.hh"
+
+int main(int argc, char const *argv[])
+{
+    // input_tests
+    input_test::TestIsPositiveRealNumber();
+    input_test::TestConvertToCents();
+
+    return 0;
+}
