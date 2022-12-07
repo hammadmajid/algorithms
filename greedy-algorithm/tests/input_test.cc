b6e0f0dbdd7d4ad455a3fb043b09c62c2ef6392a
hammadmajid <hm.hammadmajid@gmail.com>
2022-12-07 20:32:14 +0500

    Create input_test.cc

---
+++ greedy-algorithm/tests/input_test.cc
@@ -0,0 +1,13 @@
+#include "input_test.hh"
+#include <cassert>
+
+void input_test::TestIsPositiveRealNumber()
+{
+    // case 1
+    assert(Input::IsPositiveRealNumber("15") == true);
+
+}
+
+void input_test::TestConvertToCents()
+{
+}
